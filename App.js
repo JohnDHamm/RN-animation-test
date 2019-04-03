8eabe9a682cd600ffbb6f25f8df8fab60fac13d2
John Hamm <johndhamm1@gmail.com>
2019-04-03 13:28:52 -0500

    successful test for timer button animations

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, ScrollView } from 'react-native';
+
+import Timer from './timer'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <ScrollView contentContainerStyle={styles.scrollView}>
+          <Timer/>
+        </ScrollView>
+      </SafeAreaView>
     );
   }
 }
@@ -15,7 +19,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
+  },
+  scrollView: {
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    backgroundColor: 'gray'
   },
 });
